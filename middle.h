042594481afcf75a2c83072361bf7e10356ff3dd
artemzimenkov <143996814+artemzimenkov@users.noreply.github.com>
2023-10-02 11:01:26 +0300

    Add files via upload

---
+++ middle.h
@@ -0,0 +1,10 @@
+#ifndef EASY_H_INCLUDED
+#define EASY_H_INCLUDED
+
+void itc_num_print(int number);
+int itc_len_num(long long number);
+int itc_sum_num(long long number);
+long long itc_multi_num(long long number);
+int itc_max_num(long long number);
+
+#endif
